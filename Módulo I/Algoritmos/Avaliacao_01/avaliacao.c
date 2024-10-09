77a56b4c4b0c65cbd0379ded4d505e325cfa9402
brs <bernardo.rodsilva@gmail.com>
2024-10-08 22:54:38 -0300

    script: adicionando script referente a primeira avaliação

--- Módulo I/Algoritmos/Avaliacao_01/avaliacao.c
+++ Módulo I/Algoritmos/Avaliacao_01/avaliacao.c
@@ -1,23 +1,52 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include <locale.h>
-#include <stdlib.h>
 
-int main() {
+#define NUMERO_DE_OVOS_DE_PASCOA 4
+#define VALOR_MINIMO 0
 
-  double ovoDePascoa_1, ovoDePascoa_2, ovoDePascoa_3, ovoDePascoa_4;
-  printf("\nInsira o valor do primeiro ovo: ");
-  scanf("%f", &ovoDePascoa_1);
-  
-  printf("\nInsira o valor do segundo ovo: ");
-  scanf("%f", &ovoDePascoa_2);
-  
-  printf("\nInsira o valor do terceiro ovo: ");
-  scanf("%f", &ovoDePascoa_3);
-  
-  printf("\nInsira o valor do quarto ovo: ");
-  scanf("%f", &ovoDePascoa_4);
+float mediaDosPrecos(float precoOvosDePascoa[]) {
+  float soma = 0;
+  for(int i = 0; i < NUMERO_DE_OVOS_DE_PASCOA; i++) {
+    soma += precoOvosDePascoa[i];
+  }
 
-  
+  return soma / NUMERO_DE_OVOS_DE_PASCOA;
+}
+
+int min(float precoOvosDePascoa[]) {
+  int indexDoOvoMaisBarato = 0;
+
+  for(int i = 1; i < NUMERO_DE_OVOS_DE_PASCOA; i++) {
+    if(precoOvosDePascoa[i] < precoOvosDePascoa[indexDoOvoMaisBarato]) {
+      indexDoOvoMaisBarato = i;
+    }
+  }
+
+  return indexDoOvoMaisBarato;
+}
+
+int main() {  
+  setlocale(LC_ALL, "Portuguese");
+
+  float precoOvosDePascoa[NUMERO_DE_OVOS_DE_PASCOA];
+  int index = 0;
+
+  printf("Exemplo de entrada: R$ 3,2");
+
+  while(index < NUMERO_DE_OVOS_DE_PASCOA) {
+  printf("\nInsira o valor do %dº ovo de páscoa: R$ ", index + 1);
+  scanf("%f", &precoOvosDePascoa[index]);
+    if(precoOvosDePascoa[index] <= VALOR_MINIMO) {
+      printf("\nValor inválido! O valor do ovo de páscoa deve ser maior que zero.");
+      continue;
+    } else {
+      index++;
+    }
+  }
+
+  printf("\nA média dos preços dos ovos de páscoa é: R$%.2f", mediaDosPrecos(precoOvosDePascoa));
+  printf("\nO ovo de páscoa mais barato é o %dº, e ele custa: R$%.2f", min(precoOvosDePascoa) + 1, precoOvosDePascoa[min(precoOvosDePascoa)]);
 
   return 0;
 }
